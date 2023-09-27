8b5aa7cb1cd6f3767f4280e6292a3145a72e18b1
Murad <145739695+Murgitadd@users.noreply.github.com>
2023-09-28 00:05:38 +0400

    Experimental Tries Task5.cs
    Could not come up with a solution...

--- Task5.cs
+++ Task5.cs
@@ -1,1 +1,19 @@
-:(
+Console.WriteLine("Enter a number:");
+int num = int.Parse(Console.ReadLine());
+
+int high = highDig(num);
+int low = lowDig(num);
+
+int sum = highDig + lowDig;
+
+Console.WriteLine(sum);
+
+int highDig(int num)
+{
+    int hig = lowDig;
+    
+    while (num > 0)
+    {
+
+    }
+}
